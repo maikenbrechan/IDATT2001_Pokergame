012be265b92350ad23a2350b0d85e8af4b2804fc
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2021-04-05 13:15:45 +0200

    Completed task 1. Plus connected to scenebuilder, and downloaded images

--- src/main/java/edu/ntnu/oblig/PrimaryController.java
+++ src/main/java/edu/ntnu/oblig/PrimaryController.java
@@ -2,6 +2,7 @@ package edu.ntnu.oblig;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
@@ -9,4 +10,11 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    public void dealHand(){
+        tf.setText("hand dealt");
+    }
+
+    @FXML
+    private TextField tf;
 }
